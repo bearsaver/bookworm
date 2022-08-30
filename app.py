d6cdc9f28b8e50ed94ed1174d80b6ccc7a545811
root <root@LENOVO-LAPTOP.localdomain>
2022-08-30 12:58:34 -0400

    navbar skeleton

--- app.py
+++ app.py
@@ -1,1 +1,53 @@
-import Flask
+# import modules
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy import create_engine
+import error
+
+# initiate flask app
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# make sure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# initiate sql database
+engine = create_engine("sqlite:///bookworm.db")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("layout.html")
+    elif request.method == "POST":
+        return render_template("login.html")
+    else:
+        return render_template("error.html", link=error(""))
+
+@app.route("/register", methods=["GET", "POST"])
+def register:
+    #TODO
+
+@app.route("/")
+def home():
+    if session.get("user_id") is None:
+            return redirect("/login")
+
+    
