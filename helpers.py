bb21050f4e9bf1dfce085159cec8857b9d2d511b
Kate Friedman <kate.friedman.2006@gmail.com>
2022-08-31 18:06:56 -0400

    Error function

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 def error(message):
     for char in message:
         if char == " ":
             char = "_"
     # memegen.link
-    return (f"https://api.memegen.link/images/sadfrog/Error/{message}.png?width=600&height=600")
+    return render_template("error.html", link=(f"https://api.memegen.link/images/sadfrog/Error/{message}.png?width=600&height=600"))
