f05682ecb88eaec9ee8e6cafbe1eb90e59d0361c
Kate Friedman <kate.friedman.2006@gmail.com>
2022-08-31 20:11:14 -0400

    Finished login

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from sqlalchemy import create_engine
 from helpers import error
+import cs50
 
 # initiate flask app
 app = Flask(__name__)
@@ -28,7 +28,7 @@ def after_request(response):
     return response
 
 # initiate sql database
-engine = create_engine("sqlite:///bookworm.db")
+db = cs50.SQL("sqlite:///bookworm.db")
 
 
 
@@ -36,25 +36,67 @@ engine = create_engine("sqlite:///bookworm.db")
 def login():
     if request.method == "GET":
         return render_template("login.html")
+    
     elif request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
 
-        # validate user input
-        if not username or not password:
-            return error("required fields not filled")
+        print(f"username: {username} password: {password}")
 
-        return render_template("index.html")
+        username_db = db.execute("SELECT * FROM users WHERE username = ?", username)
+
+        #validate user input
+        if username == None or password == None:
+            return error("required fields not filled")
+        if username_db == []:
+            return error("username not found")
+        
+        user_id = db.execute("SELECT id FROM users WHERE username = ?", username)[0]["id"]
+        session["user_id"] = user_id
+
+        return redirect("/")
     else:
         return error("")
 
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session["user_id"] = None
+    return redirect("/login")
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    print(error(""))
-    return error("")
+    if request.method == "GET":
+        return render_template("register.html")
+
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_confirm = request.form.get("password_confirm")
+
+        # validate user input
+        if username == None or password == None or password_confirm == None:
+            return error("required fields not filled")
+        if password != password_confirm:
+            return error("passwords don't match")
+
+        # ensure username is unique
+        existing_usernames = []
+        for user in db.execute("SELECT username FROM users"):
+            existing_usernames.append(user["username"])
+
+        if username in existing_usernames:
+            return error("username taken")
+
+        # add to database
+        db.execute("INSERT INTO users (username, password) VALUES (?, ?)", username, generate_password_hash(password))
+
+        return redirect("/")
+
+    else:
+        return error("")
 
 @app.route("/")
 def home():
     if session.get("user_id") == None:
         return redirect("/login")
-    return error("")
+    return render_template("index.html")
